5b9206f37e06d57cc0a1c4ae398592071d3d3969
SuperDuperBlade <catmasterking@gmail.com>
2023-07-18 16:26:30 +0100

    Added a new Util class with a function to retrive bufferedImages form src

---
+++ src/com/SuperDuperBlade/Utils/Util.java
@@ -0,0 +1,19 @@
+package com.SuperDuperBlade.Utils;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public  class Util {
+
+    public static   BufferedImage getImageFromSource(String location){
+        try {
+            return  ImageIO.read(Util.class.getResourceAsStream(location));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+}
