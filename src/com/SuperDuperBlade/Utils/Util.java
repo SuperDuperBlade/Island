5c0264f94cb42e17d6d2093ea28307876d403c6e
SuperDuperBlade <catmasterking@gmail.com>
2023-07-19 09:30:27 +0100

    Fixed a bug where the image was not found

--- src/com/SuperDuperBlade/Utils/Util.java
+++ src/com/SuperDuperBlade/Utils/Util.java
@@ -3,11 +3,13 @@ package com.SuperDuperBlade.Utils;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public  class Util {
 
-    public static   BufferedImage getImageFromSource(String location){
+    public static BufferedImage getImageFromSource(String location){
+
         try {
             return  ImageIO.read(Util.class.getResourceAsStream(location));
         } catch (IOException e) {
