e8241743492964a3b6838832ed3e1ddab8f8db63
SuperDuperBlade <catmasterking@gmail.com>
2023-07-20 12:07:12 +0100

    Added a way for commands to be inputed into the game

--- src/com/SuperDuperBlade/Scene/GUI/Command.java
+++ src/com/SuperDuperBlade/Scene/GUI/Command.java
@@ -2,11 +2,18 @@ package com.SuperDuperBlade.Scene.GUI;
 
 import com.SuperDuperBlade.Main;
 import com.SuperDuperBlade.Managers.KeyManager;
+import com.SuperDuperBlade.Scene.World;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
 
 public class Command extends Gui{
+
+    private World world;
+
+    public Command(World world) {
+        this.world = world;
+    }
 
     private boolean toggled = false;
 
@@ -20,14 +27,37 @@ public class Command extends Gui{
 
 
         if (toggled){
-            Main.getWindow().getSceneManager().getScence().paused = true;
+            Main.getWindow().getScence().paused = true;
             g2.setColor(new Color(9,2,0,127));
             g2.fillRect(0, getWindow().getScreenSizeY() -(5*getWindow().getScale()), getWindow().getScreenSizeX(),getWindow().getScreenSizeY());
             g2.setColor(Color.white);
             g2.drawString(">"+keyManager.getMessage(),5, getWindow().getScreenSizeY() - (getWindow().getScale()));
         }else {
-            Main.getWindow().getSceneManager().getScence().paused = false;
+            Main.getWindow().getScence().paused = false;
         }
 
     }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        if (e.getKeyChar()==KeyEvent.VK_ENTER){
+            parseMessage();
+        }
+    }
+
+    public void parseMessage(){
+        String[] message = keyManager.getMessage().split(" ");
+        //todo add responses to commands
+        try {
+            switch (message[0]){
+                case "player.speed":
+                    world.getCamera().getSelectedEntity().setMovementSpeed(Integer.parseInt(message[1]));
+                    break;
+            }
+        }catch (Exception e){
+
+        }
+
+    }
+
 }
